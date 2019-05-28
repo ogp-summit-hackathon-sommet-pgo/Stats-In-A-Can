1e3c62a363e016522c07664c28f4522672a83075
harrisonx <engcxia@gmail.com>
2019-05-28 15:24:19 -0400

    new build

--- frontend/src/components/aboutUs.jsx
+++ frontend/src/components/aboutUs.jsx
@@ -28,26 +28,41 @@ const AboutUs = () => {
                         <div className={styles.aboutUsBigContainer}>
                             <div className={styles.aboutUsContainer}>
                                 <div className={styles.teammateName}>
-                                    Hello World Name
+                                    Chuangxin (Harrison) Xia
                                 </div>
                                 <div className={styles.notes}>
-                                    Hello World
+                                    Contact: chuangxin_xia [AT] sfu.ca<br/>
+                                    Data Science co-op Student at StatsCan. <br/>
+                                    Master of Computer Science in Big Data at Simon Fraser University <br/>
+                                    Tech used for this project: <br/>
+                                    • JavaScript: ReactJS <br/>
+                                    • RechartJS<br/>
+                                    • Chartjs <br/>
+                                    • Python<br/>
                                 </div>
                             </div>
                             <div className={styles.aboutUsContainer}>
                                 <div className={styles.teammateName}>
-                                    Hello World Name
+                                    Denise Chen
                                 </div>
                                 <div className={styles.notes}>
-                                    Hello World
+                                    Contact: denise_chen [AT] sfu.ca <br/>
+                                    Currently studying in SFU Big Data program and do data-science co-op in StatsCan. <br/>
+                                    Motivated to provide more analysis and make predictions on collected dataset. <br/>
+                                    Tech used for this project: <br/>
+                                    Used pandas to do data preprocessing and plotly for data visualization
                                 </div>
                             </div>
                             <div className={styles.aboutUsContainer}>
                                 <div className={styles.teammateName}>
-                                    Hello World Name
+                                    Yuetong Liu
                                 </div>
                                 <div className={styles.notes}>
-                                    Hello World
+                                    Contact: yuetong.liu [AT] alumni.ubc.ca <br/>
+                                    Data Science Co-op Student in Statistics Canada <br/>
+                                    3rd year Statistics Student in University of British Columbia <br/>
+                                    Expertise: Predictive Modelling, Sampling Survey, Data Visualization, Data Warehousing <br/>
+                                    Division of Labor: Analyze transportation performance within cities using R.<br/>
                                 </div>
                             </div>
                         </div>
